09a63181ff497a5ef05b21a5c01f74aa12da6c77
jomrs <jonathanmotareis@gmail.com>
2021-07-30 09:32:26 -0300

    adicionado command para imagens. editado de startswith -> include. sk8.

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const falas_bot = require('./messages/falas.json');
+const msg_imagens = require('./commands/msg_imagens.json');
 require('dotenv').config();
 
 const client = new Discord.Client();
@@ -11,9 +12,14 @@ client.on("ready", () => {
 // HANDLER DE MENSAGENS
 Object.keys(falas_bot).forEach((fala) => {
 	client.on("message", (mensagem) => {
-		if(mensagem.content.startsWith(fala)) {
-			mensagem.channel.send(falas_bot[fala]);
-		}
+		if(mensagem.content.includes(fala)) mensagem.channel.send(falas_bot[fala]);
+	});
+});
+
+// HANDLER DE COMANDOS IMAGENS
+Object.keys(msg_imagens).forEach((fala) => {
+	client.on("message", (mensagem) => {
+		if(mensagem.content.includes(fala)) mensagem.channel.send({files: msg_imagens[fala]});
 	});
 });
 
