77d02802e7a211dc54fb29286dcd6668fa4a483e
jomrs <Jonathanmotareis@gmail.com>
2021-11-01 17:21:23 -0300

    fix: typo on buzz module.

--- commands/buzz.js
+++ commands/buzz.js
@@ -2,7 +2,7 @@ const BUZZ_IMAGE = '../src/pictures/buzz.png'
 
 const sendBuzz = (client) => {
     client.on("message", (message) => {
-        if(mensage.author.bot) return;
+        if(message.author.bot) return;
         if(message.content.startsWith('!ele')) {
             message.channel.send({files: BUZZ_IMAGE})
         }
