390c2cd6a80af3df4b67ba96b757578f0ecb77b7
jomrs <Jonathanmotareis@gmail.com>
2021-11-01 17:27:51 -0300

    chore: changed buzz command.

--- commands/buzz.js
+++ commands/buzz.js
@@ -3,7 +3,7 @@ const BUZZ_IMAGE = '../src/pictures/buzz.png'
 const sendBuzz = (client) => {
     client.on("message", (message) => {
         if(message.author.bot) return;
-        if(message.content.startsWith('!ele')) {
+        if(message.content.startsWith('/ele')) {
             message.channel.send({files: BUZZ_IMAGE})
         }
     })
